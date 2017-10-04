dadd7c93850403e19a6a16d1de887e13c94623f3
Ethan Hunter <fc.spaceman@gmail.com>
2017-10-04 15:13:03 -0600

    remove sout.printlns

--- src/main/java/org/x2b/study/core/graphql/resolvers/MutationResolver.java
+++ src/main/java/org/x2b/study/core/graphql/resolvers/MutationResolver.java
@@ -1,11 +1,15 @@
 package org.x2b.study.core.graphql.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class MutationResolver implements GraphQLMutationResolver{
 
+    private static final Log log = LogFactory.getLog(MutationResolver.class);
+
     public String logAString(String str) {
-        System.out.println(str);
+        log.info(str);
         return "done";
     }
 }
--- src/main/java/org/x2b/study/core/graphql/types/Hello.java
+++ src/main/java/org/x2b/study/core/graphql/types/Hello.java
@@ -1,17 +1,21 @@
 package org.x2b.study.core.graphql.types;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.x2b.study.core.graphql.errors.UnauthorizedException;
 
 import java.util.concurrent.CompletableFuture;
 
 public class Hello {
 
+    private static final Log log = LogFactory.getLog(Hello.class);
+
     private String value;
 
     public Hello(String foo) {
         CompletableFuture<String> whatever;
         if (foo != null) {
-            System.out.println(foo); //TODO: don't system.out in production
+            log.info(foo);
         }
         this.value = "rip";
     }
