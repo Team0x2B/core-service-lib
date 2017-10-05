d7946cdd3a617170bcec8d52cbf1aa0db898daab
Ethan Hunter <fc.spaceman@gmail.com>
2017-10-04 18:44:23 -0600

    switch to fetcher based pattern

--- src/main/java/org/x2b/study/core/graphql/resolvers/MutationResolver.java
+++ src/main/java/org/x2b/study/core/graphql/resolvers/MutationResolver.java
@@ -1,6 +1,7 @@
 package org.x2b.study.core.graphql.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import graphql.schema.DataFetchingEnvironment;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
